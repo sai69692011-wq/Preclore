33f438b7afe4abb9216bd605a407248fa384558d
sai69692011-wq <sai69692011@gmail.com>
2026-08-02 07:27:06 +0530

    Change allow rule to array format in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: ['/'],
         disallow: [
           '/api/',
           '/auth',
